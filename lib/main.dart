b4eb7fb596c9ca39b2682a7d39f7a4c7eadedd20
Matheus Girardi <girardi.matheus@hotmail.com>
2022-05-26 18:07:06 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main(){
+  runApp(Pokemon());
+}
+
+class Pokemon extends StatelessWidget {
+  const Pokemon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/pokedex.dart
@@ -0,0 +1,128 @@
+class Pokedex {
+ late List<Pokemon> pokemon;
+
+  Pokedex({ required this.pokemon});
+
+  Pokedex.fromJson(Map<String, dynamic> json) {
+    if (json['pokemon'] != null) {
+      // ignore: deprecated_member_use
+      pokemon = <Pokemon>[];
+      json['pokemon'].forEach((v) {
+        pokemon.add(new Pokemon.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    if (pokemon != null) {
+      data['pokemon'] = pokemon.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Pokemon {
+  int? id;
+  String? num;
+  String? name;
+  String? img;
+  late List<String> type;
+  String? height;
+  String? weight;
+  String? candy;
+  int? candyCount;
+  String? egg;
+  double? spawnChance;
+  double? avgSpawns;
+  String? spawnTime;
+  late List<double> multipliers;
+  late List<String> weaknesses;
+  late List<NextEvolution> nextEvolution;
+
+  Pokemon(
+      {required this.id,
+      required this.num,
+      required this.name,
+      required this.img,
+      required this.type,
+      required this.height,
+      required this.weight,
+      required this.candy,
+      required this.candyCount,
+      required this.egg,
+      required this.spawnChance,
+      required this.avgSpawns,
+      required this.spawnTime,
+      required this.multipliers,
+      required this.weaknesses,
+      required this.nextEvolution});
+
+  Pokemon.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    num = json['num'];
+    name = json['name'];
+    img = json['img'];
+    type = json['type'].cast<String>();
+    height = json['height'];
+    weight = json['weight'];
+    candy = json['candy'];
+    candyCount = json['candy_count'];
+    egg = json['egg'];
+    spawnChance = json['spawn_chance'];
+    avgSpawns = json['avg_spawns'];
+    spawnTime = json['spawn_time'];
+    multipliers = json['multipliers'].cast<double>();
+    weaknesses = json['weaknesses'].cast<String>();
+    if (json['next_evolution'] != null) {
+      nextEvolution = <NextEvolution>[];
+      json['next_evolution'].forEach((v) {
+        nextEvolution.add(new NextEvolution.fromJson(v));
+      });
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = id;
+    data['num'] = num;
+    data['name'] = name;
+    data['img'] = img;
+    data['type'] = type;
+    data['height'] = height;
+    data['weight'] = weight;
+    data['candy'] = candy;
+    data['candy_count'] = candyCount;
+    data['egg'] = egg;
+    data['spawn_chance'] = spawnChance;
+    data['avg_spawns'] = avgSpawns;
+    data['spawn_time'] = spawnTime;
+    data['multipliers'] = multipliers;
+    data['weaknesses'] = weaknesses;
+    if (nextEvolution != null) {
+      data['next_evolution'] =
+          nextEvolution.map((v) => v.toJson()).toList();
+    }
+
+    return data;
+  }
+}
+
+class NextEvolution {
+  String? num;
+  String? name;
+
+  NextEvolution({required this.num, required this.name});
+
+  NextEvolution.fromJson(Map<String, dynamic> json) {
+    num = json['num'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['num'] = num;
+    data['name'] = name;
+    return data;
+  }
+}
